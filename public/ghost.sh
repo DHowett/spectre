c7d9f85063fcef8631726054dcbbe5d6caca2929
Dustin L. Howett <dustin@howett.net>
2013-05-25 20:13:02 -0700

    Update ghost.sh with saurik's recommendations.

--- public/ghost.sh
+++ public/ghost.sh
@@ -7,10 +7,7 @@ lang=_auto
 if [[ ! -z $2 ]]; then
 	lang=$2
 fi
-oi=$IFS
-IFS='|'
-read code url < <(curl -fs -w '%{http_code}|%{redirect_url}' --data-urlencode text@"$1" --data-urlencode lang="$lang" http://ghostbin.com/paste/new | sed -e 's/HTTP/http/g')
-IFS="$oi"
+IFS='|' read -r code url < <(curl -fs -w '%{http_code}|%{redirect_url}' --data-urlencode text@"$1" --data-urlencode lang="$lang" http://ghostbin.com/paste/new | sed -e 's/HTTP/http/g')
 if [[ $code -ne 200 && $code -ne 303 && $code -ne 302 ]]; then
 	echo "Rejected: $code" >&2
 	exit 1
