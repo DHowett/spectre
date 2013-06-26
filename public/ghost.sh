fb4b9372c5f0e8cb8e227bc81415e1733b43c8f3
Dustin L. Howett <dustin@howett.net>
2013-06-26 01:34:27 -0700

    [script] Add ghost -U, for upgrading. curl -o x instead of curl > x. Remove tempfiles!

--- public/ghost.sh
+++ public/ghost.sh
@@ -7,6 +7,7 @@ function usage() {
 	echo "        $prog -d <paste>				- Delete <paste>" >&2
 	echo "        $prog -s <paste>				- Show <paste>" >&2
 	echo "        $prog -l					- List pastes" >&2
+	echo "        $prog -U					- Upgrade ghost.sh (this will replace $0)" >&2
 }
 
 if [[ -z $1 ]]; then
@@ -20,12 +21,28 @@ if [[ ! -d "${rcdir}" ]]; then
 fi
 
 # Look for a newer version of the script (but don't interrupt the user.)
-(
-	read -r code < <(curl -fs -w '%{http_code}' -z "$0" -o /dev/null http://ghostbin.com/ghost.sh);
-	[[ $code -eq 200 ]] && echo "There's a new version of ghost.sh available at http://ghostbin.com/ghost.sh" >&2;
-)
+upgrade=$(mktemp /tmp/ghost.XXXXXX)
+{
+	read -r code < <(curl -fs -w '%{http_code}' -z "$0" -o "${upgrade}" http://ghostbin.com/ghost.sh);
+	[[ $code -eq 200 ]] && echo "There's a new version of ghost.sh available at http://ghostbin.com/ghost.sh" >&2
+	if [[ $code -ne 200 ]]; then
+		rm "${upgrade}"
+		upgrade=
+	fi
+}
 
-while getopts "hls:u:e:d:" o; do
+function _upgrade() {
+	if [[ -z "${upgrade}" ]]; then
+		echo "It doesn't get any better than this." >&2
+		exit 1
+	fi
+	mv "${upgrade}" "${0}"
+	chmod +x "${0}"
+	echo "Done." >&2
+	exit
+}
+
+while getopts "Uhls:u:e:d:" o; do
 	case $o in
 		h)
 			usage
@@ -50,12 +67,18 @@ while getopts "hls:u:e:d:" o; do
 			mode="delete"
 			paste=$OPTARG
 			;;
+		U)
+			_upgrade
+			exit
+			;;
 		?)
 			usage
 			exit 1
 			;;
 	esac
 done
+
+[[ ! -z "${upgrade}" ]] && rm "${upgrade}"
 
 shift $((OPTIND-1))
 
@@ -71,12 +94,12 @@ if [[ "${mode}" == "delete" ]]; then
 		echo "Rejected: $code" >&2
 		exit 1
 	fi
-	echo "Deleted $paste"
+	echo "Deleted $paste."
 	exit
 elif [[ "${mode}" == "edit" ]]; then
 	filename=$(mktemp /tmp/ghost.XXXXXX)
 	lang=$1
-	curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -fs http://ghostbin.com/paste/${paste}/raw > "${filename}"
+	curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -o "${filename}" -fs http://ghostbin.com/paste/${paste}/raw
 	${EDITOR:-vi} "${filename}"
 elif [[ "${mode}" == "show" ]]; then
 	curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -fs http://ghostbin.com/paste/${paste}/raw
@@ -102,6 +125,8 @@ url="http://ghostbin.com/paste/new"
 [[ "${mode}" == "edit" || "${mode}" == "update" ]] && url="http://ghostbin.com/paste/${paste}/edit"
 
 IFS='|' read -r code url < <(curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -fs -w '%{http_code}|%{redirect_url}' --data-urlencode text@"$filename" ${lang:+--data-urlencode} ${lang:+lang="$lang"} "${url}" | sed -e 's/HTTP/http/g')
+[[ "${mode}" == "edit" ]] && rm "${filename}"
+
 if [[ $code -ne 200 && $code -ne 303 && $code -ne 302 ]]; then
 	echo "Rejected: $code" >&2
 	exit 1
