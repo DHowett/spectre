9081338b9a1d02509d5ded93f2fb6a0a2ae99a33
Dustin L. Howett <dustin@howett.net>
2013-06-26 00:50:14 -0700

    [script] edit/delete/show/list pastes with ghost.sh!

--- public/ghost.sh
+++ public/ghost.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+function usage() {
+	prog=$(basename "$0")
+	echo "Syntax: $prog <filename> [language]" >&2
+	echo "        $prog -e <paste> <filename> [language]		- Update <paste>" >&2
+	echo "        $prog -d <paste>				- Delete <paste>" >&2
+	echo "        $prog -s <paste>				- Show <paste>" >&2
+	echo "        $prog -l					- List pastes" >&2
+}
+
 if [[ -z $1 ]]; then
-	echo "Syntax: $0 <filename> [lang]" >&2
+	usage
 	exit 1
 fi
 
@@ -15,6 +24,55 @@ fi
 	[[ $code -eq 200 ]] && echo "There's a new version of ghost.sh available at http://ghostbin.com/ghost.sh" >&2;
 )
 
+while getopts "hls:e:d:" o; do
+	case $o in
+		h)
+			usage
+			exit
+			;;
+		l)
+			mode="list"
+			;;
+		s)
+			mode="show"
+			paste=$OPTARG
+			;;
+		e)
+			mode="edit"
+			paste=$OPTARG
+			;;
+		d)
+			mode="delete"
+			paste=$OPTARG
+			;;
+		?)
+			usage
+			exit 1
+			;;
+	esac
+done
+
+shift $((OPTIND-1))
+
+if [[ "${mode}" == "delete" ]]; then
+	IFS='|' read -r code < <(curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -fs -w '%{http_code}' --data-urlencode "(no body)" http://ghostbin.com/paste/${paste}/delete)
+	if [[ $code -ne 200 && $code -ne 303 && $code -ne 302 ]]; then
+		echo "Rejected: $code" >&2
+		exit 1
+	fi
+	echo "Deleted $paste"
+	exit
+elif [[ "${mode}" == "show" ]]; then
+	curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -fs http://ghostbin.com/paste/${paste}/raw
+	exit
+elif [[ "${mode}" == "list" ]]; then
+	IFS=' ' read -a pastes < <(curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -fs http://ghostbin.com/session/raw)
+	for i in "${pastes[@]}"; do
+		echo "$i: http://ghostbin.com/paste/$i"
+	done
+	exit
+fi
+
 pboard=
 [[ -z "${pboard}" ]] && type pbcopy &> /dev/null && pboard=pbcopy
 [[ -z "${pboard}" ]] && type xclip &> /dev/null && [[ -n "${DISPLAY}" ]] && pboard=xclip
@@ -23,7 +81,11 @@ lang=text
 if [[ ! -z $2 ]]; then
 	lang=$2
 fi
-IFS='|' read -r code url < <(curl -c "${rcdir}/cookie.jar" -fs -w '%{http_code}|%{redirect_url}' --data-urlencode text@"$1" --data-urlencode lang="$lang" http://ghostbin.com/paste/new | sed -e 's/HTTP/http/g')
+
+url="http://ghostbin.com/paste/new"
+[[ "${mode}" == "edit" ]] && url="http://ghostbin.com/paste/${paste}/edit"
+
+IFS='|' read -r code url < <(curl -c "${rcdir}/cookie.jar" -b "${rcdir}/cookie.jar" -fs -w '%{http_code}|%{redirect_url}' --data-urlencode text@"$1" --data-urlencode lang="$lang" "${url}" | sed -e 's/HTTP/http/g')
 if [[ $code -ne 200 && $code -ne 303 && $code -ne 302 ]]; then
 	echo "Rejected: $code" >&2
 	exit 1
