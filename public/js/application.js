ea9a9fa26b2708755dbb3860809a7a60f312a7a4
Dustin L. Howett <dustin@howett.net>
2013-07-24 22:54:11 -0700

    Visual restyle.
    - delete modal dialog
    - javascript media queries for repositioning elements
    - cleaner CSS!

---
+++ public/js/application.js
@@ -0,0 +1,27 @@
+$(function() {
+	var controls = $("#paste-controls");
+	var langbox = $("#langbox");
+	if(!controls) return;
+
+	if(langbox) langbox.select2();
+
+	var mql = window.matchMedia("screen and (max-width: 767px)");
+	var lastMqlMatch = undefined;
+	var mqlListener = function(mql) {
+		if(mql.matches === lastMqlMatch) return;
+		controls.detach();
+		var newParent = undefined;
+		if(mql.matches) {
+			newParent = $("#phone-paste-control-container");
+		} else {
+			newParent = $("#desktop-paste-control-container");
+		}
+		newParent.empty();
+		newParent.append(controls);
+
+		lastMqlMatch = mql.matches;
+	};
+	mqlListener(mql);
+
+	mql.addListener(mqlListener);
+});
