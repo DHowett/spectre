af8d8180aec9b5b3505762dddabfbadb95fb31ac
Dustin L. Howett <dustin@howett.net>
2013-08-10 22:57:53 -0700

    Clean up the javascript somewhat.

--- public/js/application.js
+++ public/js/application.js
@@ -1,24 +1,40 @@
 /* From http://stackoverflow.com/questions/4217962/scroll-to-an-element-using-jquery */
-jQuery.fn.scrollMinimal = function() {
-	var cTop = this.offset().top;
-	var cHeight = this.outerHeight(true);
-	var windowTop = $(window).scrollTop();
-	var visibleHeight = $(window).height();
+(function($){
+	$.fn.fillWithLineNumbers = function(lines, callback) {
+		var lineNumberTrough = $(this[0])
+		if(lines == lineNumberTrough.data("lines")) return;
 
-	if (cTop < windowTop) {
-		$(window).scrollTop(cTop);
-	} else if (cTop + cHeight > windowTop + visibleHeight) {
-		$(window).scrollTop(cTop - visibleHeight + cHeight);
-	}
-};
+		var n="";
+		var i = 0;
+		for(i=0; i < lines; i++) {
+			n += "<span>"+(i+1)+"</span>";
+		}
+		lineNumberTrough.html(n);
+
+		lineNumberTrough.data("lines", lines);
+		if(callback) callback();
+	};
+	$.fn.scrollMinimal = function() {
+		var cTop = this.offset().top;
+		var cHeight = this.outerHeight(true);
+		var windowTop = $(window).scrollTop();
+		var visibleHeight = $(window).height();
+
+		if (cTop < windowTop) {
+			$(window).scrollTop(cTop);
+		} else if (cTop + cHeight > windowTop + visibleHeight) {
+			$(window).scrollTop(cTop - visibleHeight + cHeight);
+		}
+	};
+})(jQuery);
 
 $(function() {
 	(function(){
 		var controls = $("#paste-controls");
 		var langbox = $("#langbox");
-		if(!controls) return;
+		if(controls.length == 0) return;
 
-		if(langbox) {
+		if(langbox.length > 0) {
 			langbox.select2();
 			langbox.select2("val", langbox.data("selected"));
 		}
@@ -46,7 +62,7 @@ $(function() {
 	})();
 	(function(){
 		var encModal = $("#encryptModal")
-		if(!encModal) return;
+		if(encModal.length == 0) return;
 
 		encModal.modal({show: false});
 
@@ -72,7 +88,7 @@ $(function() {
 	})();
 	(function(){
 		var expModal = $("#expireModal")
-		if(!expModal) return;
+		if(expModal.length == 0) return;
 
 		var expInput = $("input[name='expire']")
 
@@ -87,26 +103,15 @@ $(function() {
 			expModal.modal("show");
 		});
 	})();
+
+	// Common for the following functions.
+	var lineNumberTrough = $("#line-numbers");
+	var code = $("#code"), codeeditor = $("#code-editor");
+
 	(function(){
-		var ln=$("#line-numbers");
-		if(!ln) return;
+		if(lineNumberTrough.length == 0) return;
 
-		var curLines = -1;
-
-		var fillForLines = function(lines, callback) {
-			if(lines == curLines) return;
-			var n="";
-			var i = 0;
-			for(i=0; i < lines; i++) {
-				n += "<span>"+(i+1)+"</span>";
-			}
-			ln.html(n);
-
-			curLines = lines;
-			if(callback) callback();
-		}
-
-		if($("#code").length > 0) {
+		if(code.length > 0) {
 			var linebar = $(document.createElement('div'))
 					.addClass("line-highlight-bar")
 					.hide()
@@ -115,64 +120,80 @@ $(function() {
 					.clone()
 					.addClass("line-highlight-bar-permanent")
 					.appendTo("body");
+
 			var positionLinebar = function(linebar) {
 				linebar
-					.css("left", ln.outerWidth())
+					.css("left", lineNumberTrough.outerWidth())
 					.css("top", $(this).position().top + $(this).parent().position().top)
-					.width($("#code").outerWidth())
+					.width(code.outerWidth())
 					.show();
 			}
-			fillForLines(($("#code").text().match(/\n/g)||[]).length+1, function() {
-				ln.children().mouseenter(function() {
+
+			var setSelectedLineNumber = function(line) {
+				if(typeof line != 'undefined') {
+					permabar.data("cur-line", line);
+					history.replaceState({"line":line}, "", "#L"+line);
+				} else {
+					permabar.removeData("cur-line");
+					history.replaceState(null, "", "#");
+				}
+			}
+
+			var lineFromHash = function(hash) {
+				if(!hash) return undefined;
+				var v = hash.match(/^#L(\d+)/);
+				if(typeof v != 'undefined' && v.length > 0) {
+					return v[1];
+				}
+				return undefined;
+			}
+
+			lineNumberTrough.fillWithLineNumbers((code.text().match(/\n/g)||[]).length+1, function() {
+				lineNumberTrough.children().mouseenter(function() {
 					positionLinebar.call(this, linebar);
 				}).mouseleave(function() {
 					linebar.hide();
 				}).click(function() {
 					var line = $(this).text();
-					if(history.state && history.state.line == line) {
+					if(permabar.data("cur-line") == line) {
+						setSelectedLineNumber(undefined);
 						permabar.hide();
-						history.replaceState({}, "", "#");
-						return
+						return;
 					}
+					setSelectedLineNumber(line);
 					positionLinebar.call(this, permabar);
-					history.replaceState({"line":line}, "", "#L" + line);
 				});
 
-				if(window.location.hash) {
-					var v = window.location.hash.match(/^#L(\d+)/);
-					if(v) {
-						var n = v[1];
-						var linespan = $("span:nth-child("+n+")", ln);
+				$(window).on("load popstate", function(e) {
+					var n = lineFromHash(window.location.hash);
+					if(n) {
+						var linespan = $("span:nth-child("+n+")", lineNumberTrough);
+						setSelectedLineNumber(n);
 						positionLinebar.call(linespan.get(0), permabar);
-						history.replaceState({"line":n}, "", "#L" + n);
 						linespan.scrollMinimal();
 					}
-				}
+				});
 			});
 			$(window).on("resize", function(e) {
-				$(permabar).width($("#code").outerWidth());
+				$(permabar).width(code.outerWidth());
 			});
 			$(document).on("media-query-changed", function() {
-				positionLinebar.call($("span:nth-child("+history.state.line+")", ln).get(0), permabar);
+				positionLinebar.call($("span:nth-child("+permabar.data("cur-line")+")", lineNumberTrough).get(0), permabar);
 			});
-		} else if($("#code-editor").length > 0) {
-			$("#code-editor").on("input propertychange", function() {
-				fillForLines(($("#code-editor").val().match(/\n/g)||[]).length+1, function() {
-					$(".textarea-height-wrapper").css("left", ln.outerWidth());
+		} else if(codeeditor.length > 0) {
+			codeeditor.on("input propertychange", function() {
+				lineNumberTrough.fillWithLineNumbers((codeeditor.val().match(/\n/g)||[]).length+1, function() {
+					$(".textarea-height-wrapper").css("left", lineNumberTrough.outerWidth());
 				});
 			}).triggerHandler("input");
 			$(document).on("media-query-changed", function() {
-				$("#code-editor").triggerHandler("input");
+				codeeditor.triggerHandler("input");
 			});
 		}
 	})();
 	(function(){
-		$('[autofocus]:not(:focus)').eq(0).focus();
-	})();
-	(function(){
-		var ce = $("#code-editor");
-		if(ce.length > 0) {
-			ce.keydown(function(e) {
+		if(codeeditor.length > 0) {
+			codeeditor.keydown(function(e) {
 				if(e.keyCode == 9) {
 					var ends = [this.selectionStart, this.selectionEnd];
 					this.value = this.value.substring(0, ends[0]) + "\t" + this.value.substring(ends[1], this.value.length);
@@ -181,5 +202,8 @@ $(function() {
 				}
 			});
 		}
+	})();
+	(function(){
+		$('[autofocus]:not(:focus)').eq(0).focus();
 	})();
 });
