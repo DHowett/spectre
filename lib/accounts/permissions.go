38b6388c69bd2761009dd8772c4ca4a3db8363a3
Dustin L. Howett <dustin@howett.net>
2016-06-19 13:09:22 -0700

    [accounts] Due to some stupid limitations, SQL drivers prefer signed ints.
    Therefore, to store 0xFFFFFFFF in a permission mask, it has to be a uint64.

--- lib/accounts/permissions.go
+++ lib/accounts/permissions.go
@@ -4,7 +4,7 @@ const (
 	UserPermissionUnknown Permission = (0)
 	UserPermissionAdmin              = (1 << (iota - 1))
 
-	UserPermissionAll Permission = ^Permission(0)
+	UserPermissionAll Permission = Permission(^uint32(0))
 )
 
 const (
@@ -12,10 +12,10 @@ const (
 	PastePermissionEdit               = (1 << (iota - 1))
 	PastePermissionGrant
 
-	PastePermissionAll Permission = ^Permission(0)
+	PastePermissionAll Permission = Permission(^uint32(0))
 )
 
-type Permission uint32
+type Permission uint64
 type PermissionScope interface {
 	Has(Permission) bool
 	Grant(Permission) error
