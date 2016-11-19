538efbcd09b910c4e349a2a37053914c2aa35ace
Dustin L. Howett <dustin@howett.net>
2016-11-18 22:01:43 -0800

    Implement Configuration (part 1: parsing)
    Configuration can be read from multiple files, and those files can
    contain template directives.
    
    Inside the templated config, {{.}} refers to the Configuration currently
    being loaded (without the current partially-parsed state.)
    
    Ref #51.

---
+++ config.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	"text/template"
+	"time"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type yamlDuration time.Duration
+
+func (d *yamlDuration) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	var s string
+	unmarshal(&s)
+	parsed, err := ParseDuration(s)
+	*d = yamlDuration(parsed)
+	return err
+}
+
+type Configuration struct {
+	Database *struct {
+		Dialect    string
+		Connection string
+	}
+
+	Web []struct {
+		Bind string
+		SSL  *struct {
+			CA          string `yaml:"ca"`
+			Certificate string `yaml:"cert"`
+			Key         string `yaml:"key"`
+		}
+		Proxied bool
+	}
+
+	Logging struct {
+		Components  map[string]bool
+		Destination struct {
+			Type string
+			Path string
+		}
+	}
+
+	Application struct {
+		ForceInsecureEncryption bool `yaml:"force_insecure_encryption"`
+		Limits                  struct {
+			PasteSize          uint         `yaml:"paste_size"`
+			PasteCache         uint         `yaml:"paste_cache"`
+			PasteMaxExpiration yamlDuration `yaml:"paste_max_expiration"`
+		}
+	}
+}
+
+func (c *Configuration) AppendFile(filename string) error {
+	tmpl, err := template.ParseFiles(filename)
+	if err != nil {
+		return err
+	}
+
+	buf := &bytes.Buffer{}
+	err = tmpl.Execute(buf, c)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal(buf.Bytes(), c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
