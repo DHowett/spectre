9044a660e6ba5a83628d483603fcb66f6343d51c
Dustin L. Howett <dustin@howett.net>
2016-11-21 19:53:14 -0800

    [views] Nil data provider should be acceptable.

--- views/data.go
+++ views/data.go
@@ -10,7 +10,17 @@ type DataProvider interface {
 	ViewValue(r *http.Request, name string) interface{}
 }
 
+func varFromNoop(vctx viewContext, name string) interface{} {
+	return nil
+}
+
 func varFromDataProvider(dp DataProvider) func(vctx viewContext, name string) interface{} {
+	// nil dp is safe; if we get one, no vars exist.
+	if dp == nil {
+		return varFromNoop
+	}
+
+	// otherwise, capture dp and use it for view value generation.
 	return func(vctx viewContext, name string) interface{} {
 		return dp.ViewValue(vctx.r, name)
 	}
