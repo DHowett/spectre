5c854aea74b4e4ce423fc42f50bec97c40ece135
Dustin L. Howett <dustin@howett.net>
2016-12-24 22:50:25 -0800

    [model] Remove some more cruft.

--- model/util.go
+++ model/util.go
@@ -11,15 +11,3 @@ type writeCloser struct {
 	io.Writer
 	io.Closer
 }
-
-type _devZero struct{}
-
-func (z *_devZero) Read(p []byte) (n int, err error) {
-	return 0, io.EOF
-}
-
-func (z *_devZero) Close() error {
-	return nil
-}
-
-var devZero = &_devZero{}
