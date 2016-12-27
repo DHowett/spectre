796345b81287325fc01e74a6071d9005da917f57
Dustin L. Howett <dustin@howett.net>
2016-12-26 17:17:57 -0800

    [emigre] Clean up BulkInserter.

--- cmd/emigre/bulk.go
+++ cmd/emigre/bulk.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"strings"
 
@@ -12,11 +13,11 @@ type BulkInserter struct {
 
 	// query as split by splitQuery
 	begin, placeholders, end string
-	count, limit             int
-	values                   []interface{}
+	stride, limit            int
+	vals                     []interface{}
 
 	// map [number of elements] -> query
-	preparedStatements map[int]*sqlx.Stmt
+	preparedStatements map[int]*sql.Stmt
 }
 
 // splitQuery splits a query of the form INSERT INTO x(a, b, c) VALUES (?, ?, ?) into
@@ -49,91 +50,85 @@ func NewBulkInserter(db *sqlx.DB, query string) *BulkInserter {
 		db:                 db,
 		begin:              begin,
 		placeholders:       placeholders,
-		count:              strings.Count(placeholders, "?"),
+		stride:             strings.Count(placeholders, "?"),
 		limit:              30000, // psql allows 34000?
 		end:                end,
-		preparedStatements: make(map[int]*sqlx.Stmt),
+		preparedStatements: make(map[int]*sql.Stmt),
 	}
 }
 
-func (b *BulkInserter) flush(vals []interface{}) error {
-	if len(vals)%b.count != 0 {
-		return fmt.Errorf("bulk: asked to insert %d values with a stride of %d?", len(vals), b.count)
-	}
-
-	query, ok := b.preparedStatements[len(vals)]
+func (b *BulkInserter) preparedStmtForCount(n int) (*sql.Stmt, error) {
+	stmt, ok := b.preparedStatements[n]
 	if !ok {
 		q := b.begin
-		p := make([]string, len(vals)/b.count)
+		p := make([]string, n/b.stride)
 		for i, _ := range p {
 			p[i] = b.placeholders
 		}
 		q += strings.Join(p, ",")
 		q += b.end
 		var err error
-		query, err = b.db.Preparex(b.db.Rebind(q))
+		stmt, err = b.db.Prepare(b.db.Rebind(q))
 		if err != nil {
-			return err
+			return nil, err
 		}
-		b.preparedStatements[len(vals)] = query
+		b.preparedStatements[n] = stmt
 	}
-	_, err := query.Exec(vals...)
+	return stmt, nil
+}
+
+func (b *BulkInserter) exec(vals []interface{}) error {
+	if len(vals)%b.stride != 0 {
+		return fmt.Errorf("bulk: asked to insert %d values with a stride of %d?", len(vals), b.stride)
+	}
+
+	stmt, err := b.preparedStmtForCount(len(vals))
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(vals...)
+
+	if err != nil {
+		// if we had an error...
+		if len(vals) > b.stride {
+			// ... and more than one record
+			n := len(vals) / b.stride
+			n = n / 2
+			mid := n * b.stride
+			errL, errR := b.exec(vals[:mid]), b.exec(vals[mid:])
+			if errL != nil && errR != nil {
+				return fmt.Errorf("bulk: split exec got left and right errors %v and %v", errL, errR)
+			}
+			if errL != nil {
+				err = errL
+			} else {
+				err = errR
+			}
+		} else {
+			// ... and only one record
+			err = fmt.Errorf("bulk: record (%v): %v", vals, err)
+		}
+	}
+
 	return err
 }
 
-func (b *BulkInserter) binarySplitFlush(vals []interface{}) error {
-	if len(vals) <= b.count {
-		// can't split any further.
-		err := b.flush(vals)
-		if err != nil {
-			return fmt.Errorf("bulk: record (%v) error %v", vals, err)
-		}
-	}
-
-	n := len(vals) / b.count
-	n = n / 2
-	mid := n * b.count
-
-	errLeft := b.flush(vals[:mid])  // insert low half
-	errRight := b.flush(vals[mid:]) // insert high half
-	if errLeft != nil {
-		// if the low half failed, partition further
-		errLeft = b.binarySplitFlush(vals[:mid])
-	}
-
-	if errRight != nil {
-		// if the high half failed, partition further
-		errRight = b.binarySplitFlush(vals[mid:])
-	}
-
-	if errLeft != nil && errRight != nil {
-		return fmt.Errorf("bulk: split flush got left and right errors %v and %v", errLeft, errRight)
-	}
-	if errLeft != nil {
-		return errLeft
-	}
-	return errRight
-}
-
 func (b *BulkInserter) Flush() error {
-	if len(b.values) == 0 {
+	if len(b.vals) == 0 {
 		return nil
 	}
 
-	err := b.flush(b.values)
-	if err != nil {
-		// binary search insert; narrow down to the single broken value
-		err = b.binarySplitFlush(b.values)
-	}
-	b.values = b.values[0:0]
+	err := b.exec(b.vals)
+	b.vals = b.vals[0:0]
 	return err
 }
 
 func (b *BulkInserter) Insert(vals ...interface{}) error {
 	var err error
-	if len(b.values)+len(vals) > b.limit {
+	if len(b.vals)+len(vals) > b.limit {
 		err = b.Flush()
 	}
-	b.values = append(b.values, vals...)
+	b.vals = append(b.vals, vals...)
 	return err
 }
