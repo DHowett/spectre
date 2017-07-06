e5799623d5dfad2179c666f42a60629ebdbcc779
Dustin L. Howett <dustin@howett.net>
2017-07-05 23:33:12 -0700

    [http] Update for 07ddb0d; split create/update; lift out update req

--- http/paste.go
+++ http/paste.go
@@ -18,6 +18,46 @@ type pasteHandler struct {
 	PermitterProvider PermitterProvider
 }
 
+func (ph *pasteHandler) pasteUpdateFromRequest(r *http.Request) (*spectre.PasteUpdate, error) {
+	body := r.FormValue("text")
+	language := r.FormValue("lang")
+	expireIn := r.FormValue("expire")
+	title := r.FormValue("title")
+
+	l := len(body)
+	if l == 0 || l > 1048576 /* TODO(DH) limit */ {
+		return nil, errors.New("bah")
+	}
+
+	pu := spectre.PasteUpdate{}
+	if body != "" {
+		pu.Body = &body
+	}
+
+	if language != "" {
+		pu.LanguageName = &language
+	}
+
+	if expireIn == "-1" {
+		pu.ExpirationTime = spectre.ExpirationTimeNever
+	} else if expireIn != "" {
+		dur, _ := ghtime.ParseDuration(expireIn)
+		/* TODO(DH)
+		if dur > time.Duration(pc.Config.Application.Limits.PasteMaxExpiration) {
+			dur = time.Duration(pc.Config.Application.Limits.PasteMaxExpiration)
+		}
+		*/
+		expireAt := time.Now().Add(dur)
+		pu.ExpirationTime = &expireAt
+	}
+
+	if title != "" {
+		pu.Title = &title
+	}
+
+	return &pu, nil
+}
+
 func (ph *pasteHandler) validMethod(w http.ResponseWriter, r *http.Request, methods ...string) bool {
 	for _, m := range methods {
 		if m == r.Method {
@@ -25,16 +65,6 @@ func (ph *pasteHandler) validMethod(w http.ResponseWriter, r *http.Request, meth
 		}
 	}
 	return false
-}
-
-func (ph *pasteHandler) validPasteRequest(w http.ResponseWriter, r *http.Request) bool {
-	body := r.FormValue("text") // TODO(DH) deduplicate
-	l := len(body)
-	if l == 0 || l > 1048576 /* TODO(DH) limit */ {
-		return false
-	}
-
-	return true
 }
 
 func (ph *pasteHandler) getBodyFromRequest(r *http.Request) (string, error) {
@@ -48,86 +78,77 @@ func (ph *pasteHandler) getPasteFromRequest(r *http.Request) (spectre.Paste, err
 	return ph.PasteService.GetPaste(r.Context(), nil, spectre.PasteID(pc[0]))
 }
 
-func (ph *pasteHandler) updatePaste(p spectre.Paste, w http.ResponseWriter, r *http.Request) {
-	if !ph.validMethod(w, r, "POST") {
-		return
-	}
-
-	if !ph.validPasteRequest(w, r) {
-		// TODO(DH) errors!
-		return
-	}
-
-	body := r.FormValue("text")
-	password := r.FormValue("password")
-	language := r.FormValue("lang")
-	expireIn := r.FormValue("expire")
-	title := r.FormValue("title")
-	encrypted := password != ""
-	_ = encrypted
-
-	// TODO(DH) disallow encryption on insecure links
-
-	newPaste := p == nil
-	permitter := ph.PermitterProvider.GetPermitterForRequest(r)
-
-	if p == nil {
-		var err error
-		p, err = ph.PasteService.CreatePaste(r.Context(), nil /* TODO(DH) cryptor */)
-		if err != nil {
-			// TODO(DH) err
-			return
-		}
-	}
-
-	perms := permitter.Permissions(spectre.PermissionClassPaste, p.GetID())
-	if newPaste {
-		perms.Grant(spectre.PastePermissionAll)
-	} else {
-		if !perms.Has(spectre.PastePermissionEdit) {
-			// TODO(DH) bounce'em
-			return
-		}
-	}
-
-	pw, err := p.Writer()
-	if err != nil {
-		// TODO(DH) err
-		return
-	}
-
-	if expireIn != "" && expireIn != "-1" {
-		dur, _ := ghtime.ParseDuration(expireIn)
-		/* TODO(DH)
-		if dur > time.Duration(pc.Config.Application.Limits.PasteMaxExpiration) {
-			dur = time.Duration(pc.Config.Application.Limits.PasteMaxExpiration)
-		}
-		*/
-		expireAt := time.Now().Add(dur)
-		p.SetExpirationTime(expireAt)
-	} else {
-		// Empty expireIn means "keep current expiration."
-		if expireIn == "-1" {
-			p.ClearExpirationTime()
-		}
-	}
-
-	p.SetLanguageName(language)
-	p.SetTitle(title)
-	io.WriteString(pw, body)
-	pw.Close()
-
-	Redirect(w, http.StatusSeeOther, fmt.Sprintf("/paste/%v", p.GetID()))
-}
-
 // TODO(DH) Move
 func Redirect(w http.ResponseWriter, status int, urlString string) {
 	w.Header().Set("Location", urlString)
 	w.WriteHeader(status)
 }
 
+// TODO(DH) Rename
+func (ph *pasteHandler) updatePasteC(p spectre.Paste, w http.ResponseWriter, pu *spectre.PasteUpdate) {
+	err := p.Update(*pu)
+	if err != nil {
+		// TODO(DH) bounce'em
+		panic(err)
+	}
+
+	Redirect(w, http.StatusSeeOther, fmt.Sprintf("/paste/%v", p.GetID()))
+}
+
+func (ph *pasteHandler) updatePaste(w http.ResponseWriter, r *http.Request) {
+	if !ph.validMethod(w, r, "POST") {
+		return
+	}
+
+	p, err := ph.getPasteFromRequest(r)
+	if err != nil {
+		// TODO(DH) errors!
+		return
+	}
+
+	pu, err := ph.pasteUpdateFromRequest(r)
+	if err != nil {
+		// TODO(DH) errors!
+		return
+	}
+
+	permitter := ph.PermitterProvider.GetPermitterForRequest(r)
+	perms := permitter.Permissions(spectre.PermissionClassPaste, p.GetID())
+	if !perms.Has(spectre.PastePermissionEdit) {
+		// TODO(DH) bounce'em
+		return
+	}
+
+	ph.updatePasteC(p, w, pu)
+}
+
 func (ph *pasteHandler) createPaste(w http.ResponseWriter, r *http.Request) {
-	ph.updatePaste(nil, w, r)
+	if !ph.validMethod(w, r, "POST") {
+		return
+	}
+
+	pu, err := ph.pasteUpdateFromRequest(r)
+	if err != nil {
+		// TODO(DH) errors!
+		return
+	}
+
+	password := r.FormValue("password")
+	encrypted := password != ""
+	_ = encrypted
+	// TODO(DH) disallow encryption on insecure links
+
+	p, err := ph.PasteService.CreatePaste(r.Context(), nil /* TODO(DH) cryptor */)
+	if err != nil {
+		// TODO(DH) err
+		return
+	}
+
+	permitter := ph.PermitterProvider.GetPermitterForRequest(r)
+	perms := permitter.Permissions(spectre.PermissionClassPaste, p.GetID())
+	perms.Grant(spectre.PastePermissionAll)
+
+	ph.updatePasteC(p, w, pu)
 }
 
 func (ph *pasteHandler) deletePaste(w http.ResponseWriter, r *http.Request) {
