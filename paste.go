07ddb0de2cc48f8f47ad46d0a0d4183b165b650c
Dustin L. Howett <dustin@howett.net>
2017-07-05 23:29:24 -0700

    Remove Set* from spectre.Paste; replace with Update

--- paste.go
+++ paste.go
@@ -6,38 +6,42 @@ import (
 	"time"
 )
 
+var expirationTimeNever time.Time
+var ExpirationTimeNever = &expirationTimeNever
+
 type PasteID string
 
 func (id PasteID) String() string {
 	return string(id)
 }
 
+type PasteUpdate struct {
+	LanguageName   *string
+	ExpirationTime *time.Time
+	Title          *string
+	Body           *string
+}
+
 type Paste interface {
 	GetID() PasteID
 
 	GetLanguageName() string
-	SetLanguageName(string)
+
+	// GetExpirationTime returns the time at which this paste will
+	// expire or nil if the paste does not expire.
+	// SetExpirationTime will set the paste to expire at the provided time.
+	GetExpirationTime() *time.Time
+
+	GetTitle() string
 
 	IsEncrypted() bool
 	GetEncryptionMethod() EncryptionMethod
 
-	// GetExpirationTime returns the time at which this paste will
-	// expire or nil if the paste does not expire.
-	GetExpirationTime() *time.Time
-
-	// SetExpirationTime will set the paste to expire at the provided time.
-	SetExpirationTime(time.Time)
-	ClearExpirationTime()
-
-	GetTitle() string
-	SetTitle(string)
-
 	GetModificationTime() time.Time
 
 	Reader() (io.ReadCloser, error)
-	Writer() (io.WriteCloser, error)
 
-	Commit() error
+	Update(PasteUpdate) error
 	Erase() error
 }
 
